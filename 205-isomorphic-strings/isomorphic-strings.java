270b4c9d473e1cd0b1e69044724c62074c5c2a22
SHUBHAM KUMAR <142377830+Shubham-kumarx@users.noreply.github.com>
2026-07-03 12:23:28 +0530

    Time: 17 ms (39.79%) | Memory: 43.9 MB (42.26%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,20 +1,47 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length() != t.length()) return false;
-        HashMap<Character, Character> map = new HashMap<>();
-        HashSet<Character> used = new HashSet<>();
-        for(int i = 0; i < s.length(); i++){
+        // HashMap<Character, Character> map = new HashMap<>();
+        // HashSet<Character> used = new HashSet<>();
+        // for(int i = 0; i < s.length(); i++){
+        //     char sChar = s.charAt(i);
+        //     char tChar = t.charAt(i);
+        //     if(map.containsKey(sChar)){
+        //         if(map.get(sChar) != tChar) return false;
+                
+        //     }else {
+        //             if(used.contains(tChar)) return false;
+        //             map.put(sChar, tChar);
+        //             used.add(tChar);
+        //         }
+        // }
+        // return true;
+
+        HashMap<Character, Character> sMap = new HashMap<>();
+        HashMap<Character, Character> tMap = new HashMap<>();
+
+                for (int i = 0; i < s.length(); i++) {
+
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
-            if(map.containsKey(sChar)){
-                if(map.get(sChar) != tChar) return false;
-                
-            }else {
-                    if(used.contains(tChar)) return false;
-                    map.put(sChar, tChar);
-                    used.add(tChar);
-                }
+
+            // Check s -> t mapping
+            if (sMap.containsKey(sChar)) {
+                if (sMap.get(sChar) != tChar)
+                    return false;
+            } else {
+                sMap.put(sChar, tChar);
+            }
+
+            // Check t -> s mapping
+            if (tMap.containsKey(tChar)) {
+                if (tMap.get(tChar) != sChar)
+                    return false;
+            } else {
+                tMap.put(tChar, sChar);
+            }
         }
+
         return true;
         
     }
