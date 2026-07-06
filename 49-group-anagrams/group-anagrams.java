e2650336638869211ca71006d1761c1d676959cb
SHUBHAM KUMAR <142377830+Shubham-kumarx@users.noreply.github.com>
2026-07-06 16:07:05 +0530

    Time: 19 ms (17.29%) | Memory: 50.5 MB (13.37%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,13 +1,40 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        // Map<String, List<String>> map = new HashMap<>();
+        // for(String str : strs){
+        //     char[] ch = str.toCharArray();
+        //     Arrays.sort(ch);
+        //     String key = new String(ch);
+        //     map.putIfAbsent(key, new ArrayList<>());
+        //     map.get(key).add(str);
+            
+        // }
+        // return new ArrayList<>(map.values());
+        // Time Complexity
+
+        // Sorting each string takes O(k log k).
+
+        // For n strings:
+
+        // Time: O(n × k log k)
+
+        // where:
+
+        // n = number of strings
+        // k = maximum length of a string
+
         Map<String, List<String>> map = new HashMap<>();
         for(String str : strs){
-            char[] ch = str.toCharArray();
-            Arrays.sort(ch);
-            String key = new String(ch);
+            int[] count = new int[26];
+            for(char ch : str.toCharArray()) count[ch - 'a']++;
+            StringBuilder sb = new StringBuilder();
+            for(int num : count){
+                sb.append('#');
+                sb.append(num);
+            }
+            String key = sb.toString();
             map.putIfAbsent(key, new ArrayList<>());
             map.get(key).add(str);
-            
         }
         return new ArrayList<>(map.values());
     }
